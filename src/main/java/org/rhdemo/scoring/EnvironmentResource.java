cd217f33d2faef6d889f72c4e34591a4e9117ab8
Bill Burke <bburke@redhat.com>
2020-03-13 16:41:56 -0400

    rename

--- src/main/java/org/rhdemo/scoring/EnvironmentResource.java
+++ src/main/java/org/rhdemo/scoring/EnvironmentResource.java
@@ -17,7 +17,7 @@ public class EnvironmentResource {
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public Map<String, Object> status() {
+    public Map<String, Object> environment() {
         Map<String, Object> status = new HashMap<>();
         status.put("CLUSTER_NAME", env.CLUSTER_NAME());
         status.put("DATAGRID_HOST", env.DATAGRID_HOST());
